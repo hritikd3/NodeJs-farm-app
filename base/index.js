26fa5d71f9c6bcc4ebedd7e1a1cc59c0266ea5dc
hritikd3 <hritikdangi3@gmail.com>
2023-03-12 16:24:14 +0530

    cards logic implemented

--- base/index.js
+++ base/index.js
@@ -1,49 +1,74 @@
-
 const fs = require("fs");
 const http = require("http");
+const url= require('url')
 
+const replaceTemplate = function (temp, product) {
+  let output = temp.replace(/{%PRODUCT_NAME%}/g, product.productName); //here /g for global
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRODUCT_CARDS%}/g, product.cards);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+  output = output.replace(/{%PRICE%}/g, product.price);
 
-const overview=fs.readFileSync(`${__dirname}/overview.html`, 'utf-8')
-const product=fs.readFileSync(`${__dirname}/product.html`, 'utf-8')
-const cards=fs.readFileSync(`${__dirname}/card.html`, 'utf-8')
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+};
+const tempOverview = fs.readFileSync(`${__dirname}/overview.html`, "utf-8");
+const tempCard = fs.readFileSync(`${__dirname}/card.html`, "utf-8");
+const tempProduct = fs.readFileSync(`${__dirname}/product.html`, "utf-8");
+
+// const data = fs.readFileSync(`./api/data.json`, "utf-8");
+
+// const objData = JSON.parse(data); //objData contains the array of object coming from api
 const data = fs.readFileSync(`./api/data.json`, "utf-8");
-
-const objData = JSON.parse(data); //objData contains the array of object coming from api
-
-
-//overview page 
+const dataObj = JSON.parse(data);
+//server
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
-  if (pathName === "/" || pathName === "/overview") {
- const cardsTemp= objData.map((el)=> replaceCards(,el))  // replaceCards is a function 
+  const { query, pathname } = url.parse(req.url, true);
 
+  // Overview page
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
 
-    res.end(overview);
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
 
-    //product page 
-  } else if (pathName === "/product") {
-     res.writeHead(200, {
-       "Content-type": "text/html"})
-     
-       res.end("this is PRODUCT    ");
+    // Product page
+  } else if (pathname === "/product") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
 
-    //api
-  }else if (pathName === "/api") {
+    // API
+  } else if (pathname === "/api") {
     res.writeHead(200, {
       "Content-type": "application/json",
-    }),
-      res.end(data);
+    });
+    res.end(data);
 
-      //PAGE NOT FOUND
+    // Not found
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
       "my-own-header": "hello-world",
     });
-    res.end(" <h3>Page not Found</h3> ");
+    res.end("<h1>Page not found!</h1>");
   }
 });
-//it takes port and ip adreess as parameter
+
+
 server.listen(5500, "127.0.0.1", () => {
-  console.log("port is listening ");
+  console.log("Listening to requests on port 5500");
 });
