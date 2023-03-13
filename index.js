f8909658e98855ea4aa4d83ba35159235e4372bd
hritikd3 <hritikdangi3@gmail.com>
2023-03-13 10:52:21 +0530

    installeed slugify dependency

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const slugify = require("slugify");
 const replaceTemplate = require("./modules/replaceTemplate"); //here we are importing the function from modules
 
-const tempOverview = fs.readFileSync(`${__dirname}/base/overview.html`, "utf-8"); //__dirname tells you the absolute path of the directory containing the currently executing file.
+const tempOverview = fs.readFileSync(
+  `${__dirname}/base/overview.html`,
+  "utf-8"
+); //__dirname tells you the absolute path of the directory containing the currently executing file.
 const tempCard = fs.readFileSync(`${__dirname}/base/card.html`, "utf-8");
 const tempProduct = fs.readFileSync(`${__dirname}/base/product.html`, "utf-8");
 
 const data = fs.readFileSync(`${__dirname}/api/data.json`, "utf-8");
 const dataObj = JSON.parse(data); //objData contains the array of object coming from api
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true })); //slugify helps in making the string more readable we use it for url address 
+console.log(slugs);
+
+
 //server
 const server = http.createServer((req, res) => {
   //  console.log(req.url)
